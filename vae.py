ed35f09ccf84645ae134e9ded1f19526c46a4e0c
justin <18606523+jlewis200@users.noreply.github.com>
2022-12-09 12:00:22 -0500

    Update models and training.

--- vae.py
+++ vae.py
@@ -6,33 +6,33 @@ import torch
 from models import Encoder, Decoder
 from torch.utils.data import DataLoader
 from torchvision.datasets import CIFAR10
-from torchvision.transforms import ToTensor
+from torchvision.transforms import ToTensor, ToPILImage
 from argparse import ArgumentParser
 from code import interact
 from PIL import Image
 
 
-lr = 0.01
-alpha = 1
+lr = 0.001
+alpha = 0.1
 beta = 1
 
 dataset = CIFAR10("data", download=True, transform=ToTensor())
 
-dataloader = DataLoader(dataset, batch_size=64, shuffle=True, num_workers=4, prefetch_factor=16)
+dataloader = DataLoader(dataset, batch_size=128, shuffle=True, num_workers=4, prefetch_factor=16)
 
 encoder = Encoder()
 decoder = Decoder()
-#optimizer = torch.optim.Adam(list(encoder.parameters()) + list(decoder.parameters()), lr=lr) 
-optimizer = torch.optim.Adam(encoder.parameters(), lr=lr) 
 
 if torch.cuda.is_available():
     encoder = encoder.cuda()
     decoder = decoder.cuda()
 
-for epoch in range(10):
-    lr *= 0.3
+for epoch in range(50):
+    if epoch % 10 == 0:
+        optimizer = torch.optim.Adam(list(encoder.parameters()) + list(decoder.parameters()), lr=lr) 
+        lr *= 0.25
 
-    for batch, _ in dataloader:
+    for batch, label in dataloader:
         if torch.cuda.is_available():
             batch = batch.cuda()
 
@@ -42,18 +42,44 @@ for epoch in range(10):
         mu = torch.reshape(mu, (mu.shape[0], mu.shape[1], 1, 1))
         sigma = torch.reshape(sigma, (sigma.shape[0], sigma.shape[1], 1, 1))
 
-        norm_sample = torch.normal(torch.zeros_like(sigma), torch.ones_like(sigma))
-        batch_prime = decoder.forward((sigma * norm_sample) + mu)
+        norm_sample = torch.normal(torch.zeros_like(sigma), 0.1 * torch.ones_like(sigma))
+        #norm_sample = torch.ones_like(sigma)
+        #batch_prime = decoder.forward((sigma * norm_sample) + mu)
+        batch_prime = decoder.forward((sigma + norm_sample) + mu)
+        
+        loss_kl = (sigma - sigma.exp() - mu.square() + 1).mean().mul(-0.5)
+        loss_recon = (batch_prime - batch).square().mean()
 
-        loss_kl = (sigma.square().log() - sigma.square() - mu.square() + 1).sum().mul(0.5).div(sigma.numel())
-        loss_recon = (batch_prime - batch).square().sum().div(batch.numel())
-
-        loss = -alpha * loss_kl + \
+        loss = alpha * loss_kl + \
                beta * loss_recon
 
         loss.backward()
         optimizer.step()
 
         print(f"{epoch} {loss.item():.5e} {loss_kl.item():.5e} {loss_recon.item():.5e}")
+
+decoder.eval()
+encoder.eval()
+
+mu, sigma = torch.split(encoder.forward(batch), 256, dim=1)
+mu = torch.reshape(mu, (mu.shape[0], mu.shape[1], 1, 1))
+sigma = torch.reshape(sigma, (sigma.shape[0], sigma.shape[1], 1, 1))
+
+#norm_sample = torch.normal(torch.zeros_like(sigma), 0.01 * torch.ones_like(sigma))
+norm_sample = torch.ones_like(sigma)
+imgs = decoder.forward((sigma * norm_sample) + mu)
+
+for idx, img in enumerate(imgs):
+    ToPILImage()(img).save(f"/tmp/imgs/batch_{idx}.png")
+
+
+norm_sample = torch.normal(torch.zeros(64, 256, 1, 1), torch.ones(64, 256, 1, 1))
+if torch.cuda.is_available():
+    norm_sample = norm_sample.cuda()
+
+imgs = decoder.forward(norm_sample)
+
+for idx, img in enumerate(imgs):
+    ToPILImage()(img).save(f"/tmp/imgs/img_{idx}.png")
 
 interact(local=locals())
