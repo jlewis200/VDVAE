2edcc8c80ff1b337f324b04dc236087757360bbe
justin <18606523+jlewis200@users.noreply.github.com>
2022-12-09 12:00:22 -0500

    Update models and training.

--- models.py
+++ models.py
@@ -13,35 +13,57 @@ class Decoder(nn.Sequential):
     def __init__(self):
         super(Decoder, self).__init__()
 
-        kernel_size = (3, 3)
+        kernel_size = 3
+        stride = 2
         padding = 0
         output_padding = 0
         padding_mode = "reflect"
-        leaky_relu_lower = 0.0
-        flat_size = 256
+        negative_slope = 0.0
+        flat_size = 1024 
         
         #shape:  N x 256 x 1 x 1
-        self.append(nn.ConvTranspose2d(256, 128, kernel_size=2, padding=padding, output_padding=output_padding))
-        #shape:  N x 128 x 4 x 4
-        self.append(nn.ConvTranspose2d(128, 64, kernel_size=3, padding=padding, output_padding=output_padding))
-        #shape:  N x 256 x 4 x 4
-        self.append(nn.ConvTranspose2d(64, 32, kernel_size=5, padding=padding, output_padding=output_padding))
-        #shape:  N x 256 x 8 x 8
-        self.append(nn.ConvTranspose2d(32, 16, kernel_size=9, padding=padding, output_padding=output_padding))
-        #shape:  N x 256 x 16 x 16
-        self.append(nn.ConvTranspose2d(16, 3, kernel_size=17, padding=padding, output_padding=output_padding))
+        self.append(nn.ConvTranspose2d(256, 128, kernel_size=kernel_size, stride=stride, padding=padding, output_padding=output_padding))
+        self.append(nn.BatchNorm2d(128))
+        self.append(nn.LeakyReLU(negative_slope))
+
+        #shape:  N x 128 x 3 x 3
+        self.append(nn.ConvTranspose2d(128, 64, kernel_size=kernel_size, stride=stride, padding=padding, output_padding=output_padding))
+        self.append(nn.BatchNorm2d(64))
+        self.append(nn.LeakyReLU(negative_slope))
+        
+        #shape:  N x 64 x 7 x 7
+        self.append(nn.ConvTranspose2d(64, 32, kernel_size=kernel_size, stride=stride, padding=padding, output_padding=output_padding))
+        self.append(nn.BatchNorm2d(32))
+        self.append(nn.LeakyReLU(negative_slope))
+        
+        #shape:  N x 32 x 15  x 15
+        self.append(nn.ConvTranspose2d(32, 16, kernel_size=kernel_size, stride=stride, padding=padding, output_padding=output_padding))
+        self.append(nn.BatchNorm2d(16))
+        self.append(nn.LeakyReLU(negative_slope))
+        
+        #shape:  N x 16 x 31 x 31
+#        self.append(nn.ConvTranspose2d(16, 3, kernel_size=kernel_size, stride=stride, padding=padding, output_padding=output_padding))
+#        self.append(nn.BatchNorm2d(3))
+#        self.append(nn.LeakyReLU(negative_slope))
+        
         #shape:  N x 256 x 32 x 32
+
+        self.append(nn.Conv2d(16, 3, kernel_size=4, stride=1, padding=2))
+        self.append(nn.Sigmoid())
 
 
 class Encoder(nn.Module):
     def __init__(self):
         super(Encoder, self).__init__()
 
-        kernel_size = (3, 3)
+        kernel_size = 3
+        stride = 2
         padding = 1
-        padding_mode = "reflect"
-        leaky_relu_lower = 0.0
-        flat_size = 256
+        negative_slope = 0.01
+        momentum = 0.1
+        affine = True
+        bias = True
+        flat_size = 1024 
         
         self.shared = nn.Sequential()
         self.mu = nn.Sequential()
@@ -53,58 +75,39 @@ class Encoder(nn.Module):
         #W:  width of "flow" matrix
 
         #in:  N x 3 x 32 x 32
-        self.shared.append(nn.Conv2d(3, 16, kernel_size, padding=padding, padding_mode=padding_mode))
-        self.shared.append(nn.LeakyReLU(leaky_relu_lower))
-        self.shared.append(nn.Conv2d(16, 16, kernel_size, padding=padding, padding_mode=padding_mode))
-        self.shared.append(nn.LeakyReLU(leaky_relu_lower))
-        self.shared.append(nn.MaxPool2d((2, 2)))
+        self.shared.append(nn.Conv2d(3, 32, kernel_size, padding=padding, stride=stride))
+        self.shared.append(nn.BatchNorm2d(32, momentum=momentum, affine=affine))
+        self.shared.append(nn.LeakyReLU(negative_slope))
         #out:  N x 32 x 16 x 16
 
-        #in:  N x 32 x 16 x 16
-        self.shared.append(nn.Conv2d(16, 32, kernel_size, padding=padding, padding_mode=padding_mode))
-        self.shared.append(nn.LeakyReLU(leaky_relu_lower))
-        self.shared.append(nn.Conv2d(32, 32, kernel_size, padding=padding, padding_mode=padding_mode))
-        self.shared.append(nn.LeakyReLU(leaky_relu_lower))
-        self.shared.append(nn.MaxPool2d((2, 2)))
-        #out:  N x 64 x 8 x 8
-
         #in:  N x 64 x 8 x 8
-        self.shared.append(nn.Conv2d(32, 64, kernel_size, padding=padding, padding_mode=padding_mode))
-        self.shared.append(nn.LeakyReLU(leaky_relu_lower))
-        self.shared.append(nn.Conv2d(64, 64, kernel_size, padding=padding, padding_mode=padding_mode))
-        self.shared.append(nn.LeakyReLU(leaky_relu_lower))
-        self.shared.append(nn.MaxPool2d((2, 2)))
+        self.shared.append(nn.Conv2d(32, 64, kernel_size, padding=padding, stride=stride))
+        self.shared.append(nn.BatchNorm2d(64, momentum=momentum, affine=affine))
+        self.shared.append(nn.LeakyReLU(negative_slope))
         #out:  N x 128 x 4 x 4
 
         #in:  N x 128 x 4 x 4
-        self.shared.append(nn.Conv2d(64, 128, kernel_size, padding=padding, padding_mode=padding_mode))
-        self.shared.append(nn.LeakyReLU(leaky_relu_lower))
-        self.shared.append(nn.Conv2d(128, 128, kernel_size, padding=padding, padding_mode=padding_mode))
-        self.shared.append(nn.LeakyReLU(leaky_relu_lower))
-        self.shared.append(nn.MaxPool2d((2, 2)))
+        self.shared.append(nn.Conv2d(64, 128, kernel_size, padding=padding, stride=stride))
+        self.shared.append(nn.BatchNorm2d(128, momentum=momentum, affine=affine))
+        self.shared.append(nn.LeakyReLU(negative_slope))
         #out:  N x 256 x 2 x 2
 
         #in:  N x 128 x 2 x 2
-        self.shared.append(nn.Conv2d(128, 256, kernel_size, padding=padding, padding_mode=padding_mode))
-        self.shared.append(nn.LeakyReLU(leaky_relu_lower))
-        self.shared.append(nn.Conv2d(256, 256, kernel_size, padding=padding, padding_mode=padding_mode))
-        self.shared.append(nn.LeakyReLU(leaky_relu_lower))
-        self.shared.append(nn.MaxPool2d((2, 2)))
+        self.shared.append(nn.Conv2d(128, 256, kernel_size, padding=padding, stride=stride))
+        self.shared.append(nn.BatchNorm2d(256, momentum=momentum, affine=affine))
+        self.shared.append(nn.LeakyReLU(negative_slope))
         #out:  N x 256 x 1 x 1
 
         #in:  N x 256 x 1 x 1
         self.shared.append(nn.Flatten())
-        self.shared.append(nn.BatchNorm1d(flat_size))
         #out: N x 256
 
         #in: N x 256
-        self.mu.append(nn.Linear(flat_size, flat_size))
-        self.mu.append(nn.Linear(flat_size, flat_size))
+        self.mu.append(nn.Linear(flat_size, 256, bias=bias))
         #out: N x 256
 
         #in: N x 256
-        self.sigma.append(nn.Linear(flat_size, flat_size))
-        self.sigma.append(nn.Linear(flat_size, flat_size))
+        self.sigma.append(nn.Linear(flat_size, 256, bias=bias))
         #out: N x 256
 
     def forward(self, x):
