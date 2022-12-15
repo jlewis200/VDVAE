86bcd68f85b91bdbef02d077de120691e53d83ad
justin <18606523+jlewis200@users.noreply.github.com>
2022-12-15 12:02:01 -0500

    Pylint.

--- models.py
+++ models.py
@@ -1,56 +1,71 @@
 #!/usr/bin/python3
 
 """
+Variational Auto Encoder (VAE) encoder/decoder models.
 """
 
-import torch
-
 from torch import nn
-from code import interact
 
 
 class View(nn.Module):
+    """
+    Pytorch module of view change.
+    """
+
     def __init__(self, shape):
         super().__init__()
         self.shape = shape
 
-    def forward(self, input):
-        return input.view((-1, *self.shape))
+    def forward(self, tensor):
+        """
+        Return a view of the tensor in the correct shape.
+        """
+
+        return tensor.view((-1, *self.shape))
 
 
 class Decoder(nn.Sequential):
+    """
+    VAE decoder model.
+    """
+
     def __init__(self):
-        super(Decoder, self).__init__()
+        super().__init__()
 
         kernel_size = 3
         stride = 2
         padding = 1
         output_padding = 1
-        padding_mode = "reflect"
         negative_slope = 0.0
         bias = True
-        flat_size = 4096 
-        
-        #shape:  N x 1024 
+        flat_size = 4096
+
+        #shape:  N x 1024
 
         self.append(nn.Linear(1024, flat_size, bias=bias))
         self.append(nn.Dropout(p=0.2))
         self.append(nn.Linear(flat_size, flat_size, bias=bias))
-        
+
         #shape:  N x 4096
 
         self.append(View((1024, 2, 2)))
-        
+
         #shape:  N x 1024 x 2 x 2
 
         in_size = 1024
         out_size = 512
 
         for _ in range(6):
-            self.append(nn.ConvTranspose2d(in_size, out_size, kernel_size=kernel_size, stride=stride, padding=padding, output_padding=output_padding))
+            self.append(nn.ConvTranspose2d(in_size,
+                                           out_size,
+                                           kernel_size=kernel_size,
+                                           stride=stride,
+                                           padding=padding,
+                                           output_padding=output_padding))
+
             self.append(nn.BatchNorm2d(out_size))
             self.append(nn.LeakyReLU(negative_slope))
-            
+
             in_size = out_size
             out_size //= 2
             out_size = max(32, out_size)
@@ -58,17 +73,20 @@ class Decoder(nn.Sequential):
         #shape:  N x 32 x 128 x 128
 
         self.append(nn.Conv2d(32, 3, kernel_size=3, stride=1, padding=1))
-        
+
         #shape:  N x 3 x 128 x 128
-        
+
         self.append(nn.Sigmoid())
 
 
 class Encoder(nn.Module):
-    def __init__(self):
-        super(Encoder, self).__init__()
+    """
+    VAE Encoder module.
+    """
 
-        eps=1e-5
+    def __init__(self):
+        super().__init__()
+
         kernel_size = 3
         stride = 2
         padding = 1
@@ -76,22 +94,22 @@ class Encoder(nn.Module):
         momentum = 0.1
         affine = True
         bias = True
-       
+
         out_size = 1024
         flat_size = 4096
 
-        
         self.shared = nn.Sequential()
-        self.mu = nn.Sequential()
-        self.sigma = nn.Sequential()
+        self.mean = nn.Sequential()
+        self.var = nn.Sequential()
 
         in_channels = 3
-        out_channels = 32 
+        out_channels = 32
 
         #shape:  N x 3 x 128 x 128
 
         for _ in range(6):
-            self.shared.append(nn.Conv2d(in_channels, out_channels, kernel_size, padding=padding, stride=stride))
+            self.shared.append(nn.Conv2d(in_channels, out_channels, kernel_size, \
+                                         padding=padding, stride=stride))
             self.shared.append(nn.BatchNorm2d(out_channels, momentum=momentum, affine=affine))
             self.shared.append(nn.LeakyReLU(negative_slope))
 
@@ -99,25 +117,30 @@ class Encoder(nn.Module):
             out_channels *= 2
 
         #shape:  N x 1024 x 2 x 2
-        
+
         self.shared.append(nn.Flatten())
         self.shared.append(nn.Linear(flat_size, flat_size, bias=bias))
         self.shared.append(nn.Dropout(p=0.2))
-        
+
         #shape: N x 4096
 
-        #in: N x  2048
-        self.mu.append(nn.Linear(flat_size, out_size, bias=bias))
-        #out: N x 256
+        #in: N x 4096
+        self.mean.append(nn.Linear(flat_size, out_size, bias=bias))
+        #out: N x 1024
 
-        #in: N x  2048
-        self.sigma.append(nn.Linear(flat_size, out_size, bias=bias))
-        #out: N x 256
+        #in: N x 4096
+        self.var.append(nn.Linear(flat_size, out_size, bias=bias))
+        #out: N x 1024
 
-    def forward(self, x):
-        intermediate = self.shared.forward(x)
-        mu = self.mu.forward(intermediate)
-        sigma = self.sigma.forward(intermediate)
+    def forward(self, tensor):
+        """
+        Pass input tensor through the shared portion of the encoder.  Pass the intermediate output
+        through the mean/var branches.
+        """
 
-        #out (N x 256, N x 256)
-        return mu, sigma
+        intermediate = self.shared.forward(tensor)
+        mean = self.mean.forward(intermediate)
+        var = self.var.forward(intermediate)
+
+        #out (N x 1024, N x 1024)
+        return mean, var
--- vae.py
+++ vae.py
@@ -1,84 +1,146 @@
 #!/usr/bin/python3
 
+"""
+Variational Auto Encoder
+"""
+
+
 # pylint: disable=no-member
+
+from argparse import ArgumentParser
+
 import torch
+from torch.utils.data import DataLoader
+from torchvision.datasets import CelebA
+from torchvision.transforms import ToTensor, ToPILImage, Compose, Resize, RandomHorizontalFlip
+from PIL import Image
+
 
 from models import Encoder, Decoder
-from torch.utils.data import DataLoader
-from torchvision.datasets import CelebA 
-from torchvision.transforms import ToTensor, ToPILImage, Compose, Resize, RandomHorizontalFlip
-from argparse import ArgumentParser
-from PIL import Image
-from time import time
-from code import interact
+
+IMG_SIZE = (128, 128)
+
 
 def main():
+    """
+    Main function to parse command line args and initiate training/experiments.
+    """
+
     # get command line args
     parser = ArgumentParser(description="Perform VAE experiments")
 
     # options
-    mutually_exclusive = parser.add_mutually_exclusive_group()
-    mutually_exclusive.add_argument("-t", "--train", action="store_true", help="apply rotation regularization")
-    mutually_exclusive.add_argument("-p", "--pre-trained", action="store_true", help="apply rotation regularization")
-   
-    #training options
+    parser.add_argument("-t", "--train", action="store_true", help="train the model")
     parser.add_argument("-b", "--beta", type=float, default=1, help="relative weight of KL divergence loss")
     parser.add_argument("-l", "--learning-rate", type=float, default=0.0001, help="learning rate of optimizer")
-    parser.add_argument("-i", "--iterations", type=int, default=50, help="number of training iterations")
+    parser.add_argument("-i", "--iterations", type=int, default=10, help="number of training iterations")
 
     #pre-trained options
-    parser.add_argument("-e", "--encoder", type=str, default="checkpoints/encoder/encoder.pt")
-    parser.add_argument("-d", "--decoder", type=str, default="checkpoints/decoder/decoder.pt")
+    parser.add_argument("-e", "--encoder", type=str)
+    parser.add_argument("-d", "--decoder", type=str)
 
     #experiment options
-    parser.add_argument("-r", "--reconstruct", type=str, help="encode/decode an image")
-    parser.add_argument("-q", "--interpolate", type=str, nargs=2, help="interpolate between 2 images")
-    parser.add_argument("-z", "--interpolations", type=int, default=3, help="interpolate between 2 images")
-    parser.add_argument("-o", "--out", type=str, default="out.jpg", help="output file name")
+    parser.add_argument("--reconstruct", type=str, default=[], nargs="+", help="encode/decode an image")
+    parser.add_argument("--interpolate", type=str, default=[], nargs=2, help="interpolate between 2 images")
+    parser.add_argument("--interpolations", type=int, default=3, help="number of interpolations")
+    parser.add_argument("--random", type=int, help="number of random samples")
 
     args = parser.parse_args()
-    
-    if args.train:
-        encoder, decoder = train(lr=args.learning_rate, beta=args.beta, epochs=args.iterations)
 
-    else:
+    encoder, decoder = None, None
+
+    if args.encoder is not None:
         encoder = torch.load(args.encoder)
+
+    if args.decoder is not None:
         decoder = torch.load(args.decoder)
 
-    if args.reconstruct is not None:
-        img = Image.open(args.reconstruct).convert("RGB")
-        transform = Compose((ToTensor(), Resize((128, 128))))
-        img = transform(img)
-        img = img.unsqueeze(0)
-        img = reconstruct(encoder, decoder, img)
+    if args.train:
+        imgs = load_images(args.reconstruct + args.interpolate)
+        encoder, decoder = train(encoder=encoder,
+                                 decoder=decoder,
+                                 learning_rate=args.learning_rate,
+                                 beta=args.beta,
+                                 epochs=args.iterations,
+                                 imgs=imgs)
+
+    if args.reconstruct != []:
+        imgs = load_images(args.reconstruct)
+        imgs = reconstruct(encoder, decoder, imgs)
+
+        for img, filename in zip(imgs, args.reconstruct):
+            ToPILImage()(img.squeeze(0)).save(f"reconstructed_{filename}")
+
+    if args.interpolate != []:
+        img_0, img_1 = load_images(args.interpolate).split(1)
+        interpolations = interpolate(encoder,
+                                     decoder,
+                                     img_0,
+                                     img_1,
+                                     n_interpolations=args.interpolations)
+
+        montage = get_montage([img_0] + interpolations + [img_1])
+        filename_0 = args.interpolate[0].split('.')[0]
+        filename_1 = args.interpolate[1].split('.')[0]
+        montage.save(f"interpolate_{filename_0}_{filename_1}.jpg")
+
+    if args.random is not None:
+        imgs = sample(decoder, args.random)
+        montage = get_montage(imgs)
+        montage.save("random_montage.jpg")
+
+
+def load_images(img_paths):
+    """
+    Lead a list of pathnames, preprocess, return as a tensor with shape:
+    N x 3 x H x W
+    """
+
+    transform = Compose((ToTensor(), Resize(IMG_SIZE)))
+    imgs = [transform(Image.open(path).convert("RGB")).unsqueeze(0) for path in img_paths]
+    return torch.cat(imgs)
+
+
+def get_montage(imgs):
+    """
+    Build a montage from a list of PIL images.
+    """
+
+    montage = Image.new("RGB", (imgs[0].shape[-1] * len(imgs), imgs[0].shape[-2]))
+
+    for idx, img in enumerate(imgs):
         img = img.squeeze(0)
-        ToPILImage()(img).save(args.out)
-    
-    elif args.interpolate is not None:
-        transform = Compose((ToTensor(), Resize((128, 128))))
-        
-        img_0 = Image.open(args.interpolate[0]).convert("RGB")
-        img_0 = transform(img_0).unsqueeze(0)
+        img = ToPILImage()(img)
+        montage.paste(img, (idx * imgs[0].shape[-1], 0))
 
-        img_1 = Image.open(args.interpolate[1]).convert("RGB")
-        img_1 = transform(img_1).unsqueeze(0)
+    return montage
 
-        interpolations = interpolate(encoder, decoder, img_0, img_1, n_interpolations=args.interpolations)
-        interpolations = [img_0] + interpolations + [img_1]
 
-        montage = Image.new("RGB", (img_0.shape[-1] * len(interpolations), img_0.shape[-2]))
+def sample(decoder, n_samples):
+    """
+    Get a number of random samples from the decoder.
+    """
 
-        for idx, img in enumerate(interpolations):
-            img = img.squeeze(0)
-            img = ToPILImage()(img)
-            montage.paste(img, (idx * img_0.shape[-1], 0))
+    imgs = []
 
-        montage.save(args.out)
-        
+    if torch.cuda.is_available():
+        decoder = decoder.cuda()
+
+    for _ in range(n_samples):
+        random_latent = torch.rand(1, 1024)
+
+        if torch.cuda.is_available():
+            random_latent = random_latent.cuda()
+
+        img = decoder(random_latent)
+        imgs.append(img)
+
+    return imgs
 
 
 def interpolate(encoder, decoder, img_0, img_1, n_interpolations=3):
     """
+    Perform a linear interpolation between the latent encodings of one image and another.
     """
 
 
@@ -87,104 +149,127 @@ def interpolate(encoder, decoder, img_0, img_1, n_interpolations=3):
         decoder = decoder.cuda()
         img_0 = img_0.cuda()
         img_1 = img_1.cuda()
- 
+
     interpolations = []
-    
+
     encoder.eval()
     decoder.eval()
-   
+
+    img_0, _ = encoder.forward(img_0)
+    img_1, _ = encoder.forward(img_1)
+
     for idx in range(0, 1 + n_interpolations):
         ratio = idx / n_interpolations
-
         img = (img_0 * (1 - ratio)) + (img_1 * ratio)
-
-        interpolations.append(decoder.forward(encoder.forward(img)[0]))
+        interpolations.append(decoder.forward(img))
 
     return interpolations
 
 
 def reconstruct(encoder, decoder, img):
     """
+    Encode/decode an image.  Return the reconstructed tensor.
     """
 
     if torch.cuda.is_available():
         encoder = encoder.cuda()
         decoder = decoder.cuda()
         img = img.cuda()
- 
+
     encoder.eval()
     decoder.eval()
-    
+
     return decoder.forward(encoder.forward(img)[0])
 
 
-def train(encoder=None, decoder=None, lr=0.0001, beta=0.1, epochs=50):
+def train(encoder=None,
+          decoder=None,
+          learning_rate=0.0001,
+          beta=0.1,
+          epochs=50,
+          imgs=None):
     """
+    Train the model using supplied hyperparameters.  The encoder and decoder can be supplied to
+    provide the model with a warm start.
     """
-
-    size = 256
 
     #use random horizontal flip to augment the dataset
-    transform = Compose((ToTensor(), Resize((128, 128)), RandomHorizontalFlip()))
+    transform = Compose((ToTensor(), Resize(IMG_SIZE), RandomHorizontalFlip()))
     dataset = CelebA("celeba", download=True, transform=transform)
-    dataloader = DataLoader(dataset, batch_size=192, shuffle=True, num_workers=2, prefetch_factor=192)
+    dataloader = DataLoader(dataset,
+                            batch_size=192,
+                            shuffle=True,
+                            num_workers=2,
+                            prefetch_factor=192)
 
     if encoder is None:
         encoder = Encoder()
-    
-    if encoder is None:
+
+    if decoder is None:
         decoder = Decoder()
 
     if torch.cuda.is_available():
         encoder = encoder.cuda()
         decoder = decoder.cuda()
 
-    optimizer = torch.optim.Adam(list(encoder.parameters()) + list(decoder.parameters()), lr=lr) 
-
-    start_time = int(time())
+    optimizer = torch.optim.Adam(list(encoder.parameters()) + list(decoder.parameters()),
+                                 lr=learning_rate)
 
     for epoch in range(epochs):
         #save the model weights every 10 epochs
         if (1 + epoch) % 10 == 0:
-            torch.save(encoder, f"checkpoints/encoder/{start_time}_{epoch}")
-            torch.save(decoder, f"checkpoints/decoder/{start_time}_{epoch}")
+            torch.save(encoder, f"checkpoints/encoder/encoder_{epoch}")
+            torch.save(decoder, f"checkpoints/decoder/encoder_{epoch}")
 
-        for batch, label in dataloader:
+        for batch, _ in dataloader:
+            if imgs is not None:
+                batch = torch.cat((batch, imgs))
+
             if torch.cuda.is_available():
                 batch = batch.cuda()
 
-            #project the sample to the latent dimensional space
-            mu, sigma = encoder.forward(batch)
-           
-            #get reparameterized sample using mu as the mean and sigma as the standard deviation
-            std = torch.exp(sigma / 2)
-            q = torch.distributions.Normal(mu, std)
-            
-            #reparameterized sample from N(mu, std)
-            z = q.rsample()
-        
-            #reconstruct the original sample from the latent dimension representation
-            batch_prime = decoder.forward(z)
-     
-            #generate the target normal distribution of the appropriate shape
-            p = torch.distributions.Normal(torch.zeros_like(mu), torch.ones_like(mu))
-          
-            #calculate the KL divergence between N(0, 1) and N(mu, std)
-            loss_kl = (q.log_prob(z) - p.log_prob(z)).mean()
+            print(f"{epoch} ", end='')
+            train_step(encoder, decoder, optimizer, beta, batch)
 
-            #calculate the reconstruction loss
-            loss_recon = torch.nn.functional.mse_loss(batch_prime, batch)
-
-            loss = beta * loss_kl + loss_recon
-
-            #train the model weights
-            loss.backward()
-            optimizer.step()
-            optimizer.zero_grad(set_to_none=True)
-
-            print(f"{epoch} {loss.item():.5e} {loss_kl.item():.5e} {loss_recon.item():.5e}")
-        
     return encoder, decoder
+
+
+def train_step(encoder, decoder, optimizer, beta, batch):
+    """
+    Take one training step for a given batch of data.
+    """
+
+    #project the sample to the latent dimensional space
+    mean, var = encoder.forward(batch)
+
+    #get reparameterized sample using model mean as the mean and variance as the standard deviation
+    std = torch.exp(var / 2)
+    encoded_normal = torch.distributions.Normal(mean, std)
+
+    #reparameterized sample from N(mean, std)
+    encoded_rsample = encoded_normal.rsample()
+
+    #reconstruct the original sample from the latent dimension representation
+    batch_prime = decoder.forward(encoded_rsample)
+
+    #generate the target normal distribution of the appropriate shape
+    unit_normal = torch.distributions.Normal(torch.zeros_like(mean), torch.ones_like(mean))
+
+    #calculate the KL divergence between N(0, 1) and N(mean, std)
+    loss_kl = (encoded_normal.log_prob(encoded_rsample) - \
+               unit_normal.log_prob(encoded_rsample)).mean()
+
+    #calculate the reconstruction loss
+    loss_recon = torch.nn.functional.mse_loss(batch_prime, batch)
+
+    loss = beta * loss_kl + loss_recon
+
+    #train the model weights
+    loss.backward()
+    optimizer.step()
+    optimizer.zero_grad(set_to_none=True)
+
+    print(f"{loss.item():.5e} {loss_kl.item():.5e} {loss_recon.item():.5e}")
 
 
 if __name__ == "__main__":
